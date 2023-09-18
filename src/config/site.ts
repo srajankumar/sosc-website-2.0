67493582aeeaff870074886bb392ec3d60340dc9
srajan <kumarsrajan02@gmail.com>
2023-09-18 20:17:40 +0530

    metadata

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "SOSC",
+  url: "",
+  ogImage: "",
+  description: "The official SOSC website",
+  links: {
+    twitter: "https://twitter.com/sahyadri_osc",
+    github: "https://github.com/so-sc/sosc-website-2.0",
+  },
+};
