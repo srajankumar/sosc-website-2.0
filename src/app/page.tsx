65e23421731034b31ee885a3cee7043cd8679200
srajan <kumarsrajan02@gmail.com>
2023-12-02 09:54:30 +0530

    some minor improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import HorizontalScroll from "@/components/Home/HorizontalScroll";
 import TextScroll from "@/components/Home/TextScroll";
 import Hero from "@/components/Home/Hero";
 import Team from "@/components/Home/Team";
-import Line from "@/components/Home/Line";
 
 import About from "@/components/Description";
 export default function Home() {
   return (
     <div>
-      <Line />
       <Hero />
       <About />
       <HorizontalScroll />
--- src/components/Home/Team.tsx
+++ src/components/Home/Team.tsx
@@ -3,16 +3,18 @@ import Image from "next/image";
 const About = () => {
   return (
     <div className="flex justify-center flex-col my-5 py-5 m-[5%]">
-      <h1 className="font-montserratB text-4xl md:text-5xl my-5">
-        Introducing the Team
-      </h1>
-      <div className="flex md:mt-10 mt-20 justify-center">
-        <Image
-          src="/assets/home/team.png"
-          width={500}
-          height={500}
-          alt="sosc-team"
-        ></Image>
+      <div className="xl:mx-12 lg:px-4 md:px-1 px-1.5">
+        <h1 className="font-montserratB text-4xl md:text-5xl my-5">
+          Introducing the Team
+        </h1>
+        <div className="flex md:mt-10 mt-20 justify-center">
+          <Image
+            src="/assets/home/team.png"
+            width={500}
+            height={500}
+            alt="sosc-team"
+          ></Image>
+        </div>
       </div>
     </div>
   );
